73c6b307d06f61c103b9bf53b86b095281b6beae
frank <fvl7997@gmail.com>
2023-05-30 18:31:03 -0400

    cleanup

--- internal/entities/monster.go
+++ internal/entities/monster.go
@@ -1,8 +1,8 @@
 package entities
 
 type Monster struct {
-	ID         string `json:"id"`
-	CharcterID string `json:"character_id"`
-	CurrentHP  int    `json:"current_hp"`
-	Key        string `json:"key"`
+	ID          string `json:"id"`
+	CharacterID string `json:"character_id"`
+	CurrentHP   int    `json:"current_hp"`
+	Key         string `json:"key"`
 }
